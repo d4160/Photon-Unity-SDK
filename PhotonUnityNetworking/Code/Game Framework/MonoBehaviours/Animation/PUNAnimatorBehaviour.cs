8ea24bd222ce8c80565048374ddd709ad2cc28c5
d4160 <d4160@outlook.com>
2020-03-17 16:35:41 -0500

    Some additions.

---
+++ PhotonUnityNetworking/Code/Game Framework/MonoBehaviours/Animation/PUNAnimatorBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using d4160.Core;
+using Photon.Pun;
+using UnityEngine;
+
+namespace d4160.GameFramework
+{
+    public class PUNAnimatorBehaviour : AnimatorBehaviour
+    {
+        protected PhotonView _photonView;
+
+        public PhotonView PhotonView
+        {
+            get
+            {
+                if (this._photonView == null)
+                {
+                    this._photonView = PhotonView.Get(this);
+                }
+                return this._photonView;
+            }
+        }
+
+        public override void SetFloat(int paramIndex, float value)
+        {
+            if (PhotonView.IsMine == false && PhotonNetwork.IsConnected == true)
+            {
+                return;
+            }
+
+            if (_paramNames.IsValidIndex(paramIndex))
+            {
+                _animator.SetFloat(ref _paramNames[paramIndex], value);
+            }
+        }
+    }
+}
---
+++ PhotonUnityNetworking/Code/Game Framework/MonoBehaviours/PUNEntityBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using d4160.Networking;
+using Photon.Pun;
+using UnityEngine;
+
+namespace d4160.GameFramework
+{
+    public class PUNEntityBehaviour : NetworkingEntityBehaviour
+    {
+        protected PhotonView _photonView;
+
+        public PhotonView PhotonView
+        {
+            get
+            {
+                if (this._photonView == null)
+                {
+                    this._photonView = PhotonView.Get(this);
+                }
+                return this._photonView;
+            }
+        }
+
+        public override bool IsLocal => PhotonView.IsMine;
+    }
+}
